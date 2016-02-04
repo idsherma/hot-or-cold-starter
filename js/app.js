058eef0e9b5cb5a4027244a3e19256161f4a5273
IVORYSHERMAN <idsherma@gmail.com>
2016-02-03 22:33:53 -0500

    Updated Changes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,50 @@
-
 $(document).ready(function(){
+	var count = 0;
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	/*---Random Number creation method ---*/
+	var randomNum = Math.floor(Math.random() * 100) + 1;
+	$('#guessButton').click(function(){
+		var userChoice = parseInt( $('#userGuess').val() );
+		count++;
+		$('#count').text(count);	
 
-  	});
+		/*--- Number Guessing (Too low, Too high, etc.) ---*/
+		var guessNumber = function (i) {
+			if (i < randomNum) {
+				setFeedback('Too Low! Go Higher!'); 
+			}
+			else if (i > randomNum) {
+				setFeedback('Too high! Go Lower!'); 
+			}
+			else if (i == randomNum) {
+				setFeedback('Congrats! You win!');
+			}
+			else {
+				setFeedback(i);
+			}
+		}
+		guessNumber(userChoice);
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+		function setFeedback(feedback) {
+			$('#feedback').text(feedback);
+		}
 
-});
+		/*--- Log the numbers user guessed ---*/
+		$(function() {		
+		$('ul#guessList').append("<li>" + userGuess.value + "<li>");
+		})
+
+		/*--- Display information modal box ---*/
+	  	$(".what").click(function(){
+	    	$(".overlay").fadeIn(1000);
+	  	});
+
+	  	/*--- Hide information modal box ---*/
+	  	$("a.close").click(function(){
+	  		$(".overlay").fadeOut(1000);
+	  	});	  		
+	});	 
+});	
+
 
 
